a32877d4a2a8391c95e56fd2914a4016516184bc
alan <alan_ipb@apps.ipb.ac.id>
2020-11-14 00:04:37 +0700

    LETS START

--- README.md
+++
@@ -1,2 +0,0 @@
-# alansbox
- My web protfolio, in design to display all the projects I've done and/or contributed
--- main/resources/scripts/adapts.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    var navheight = $(".navbar").outerHeight();
-    var ctnheight = $("#content").height();
-    $('.content').css({
-        'padding-top' : navheight,
-        'min-height' : ctnheight - navheight,
-    });
-
-});
