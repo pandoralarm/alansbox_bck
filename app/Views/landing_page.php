15ac44a2a54b0e5b0547421fea91e7596d3b00b0
alan <alan_ipb@apps.ipb.ac.id>
2020-11-15 10:30:49 +0700

    Parallax

--- app/Views/landing_page.php
+++ app/Views/landing_page.php
@@ -26,3 +26,29 @@
         <div class="col"></div>
     </div>
 </div>
+
+<div class="container-fluid">
+    <div class="row" style="height: 40vh;">
+        <div class="col"></div>
+    </div>
+
+    <div class="row" style="height: 15vh;">
+        <div class="col"></div>
+
+        <div class="col-6 d-flex text-center" style="min-height: 50%; ">
+            <img class="thumbnail" src="images/forest.jpg" alt="image">
+        </div>
+        
+        <div class="col"></div>
+    </div>
+
+    <div class="row" style="height: 10vh;">
+        <div class="col text-center">
+            <div id="more" class="btn btn-outline-dark text-center">Find out More</div>
+        </div>
+    </div>
+
+    <div class="row" style="height: 35vh;">
+        <div class="col"></div>
+    </div>
+</div>
--- public/js/adapts.js
+++ public/js/adapts.js
@@ -63,6 +63,14 @@ $(document).ready(function () {
 
     };
 
+
+    
     //Roll Into
     intro();
+
+    var image = document.getElementsByClassName('thumbnail');
+    new simpleParallax(image, {
+        delay: .6,
+        transition: 'cubic-bezier(0,0,0,1)'
+    });
 });
